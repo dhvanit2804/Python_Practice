98a07c084524c41f77948bfdbd1263918c756a1b
dhvanit2804 <dhvanitparate123@gmail.com>
2026-02-11 11:05:18 +0530

    11-2-2026

--- 5. Sets/set.py
+++ 5. Sets/set.py
@@ -2,6 +2,29 @@
 is a built-in data type that allows you to store multiple 
 items in a single variable'''
 
-s = {1, 2, 3, 55, "Dhvanit", "Rahul", 94.4, True}
+s = {10, 20, "tops", True, "python", 1.1, 2.2, "java",10, 20, 1, 2, False, "testing"}
+s1 = {1,2,3,4,5,6,7,8,9,10,"python"}
 
-print(s, type(s))
+print(s)
+s.add(100)
+print(s)
+print(s.difference(s1))
+s.discard(1)
+print(s)
+
+'pop remove data from first'
+print(s.pop())
+print(s)
+print(s.pop())
+print(s)
+print(s.pop())
+print(s)
+
+s.remove(10)
+print(s)
+
+s2 = {1.1,2.2,3.3,4.4,5.5,6.6,7.7}
+s.update(s2)
+print(s)
+
+print()
